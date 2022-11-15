3fb25c75f11de9a9b6ca9f79447f6c2b2ff970f8
ranauzair474@gmail.com <ihtisham062@gmail.com>
2022-11-15 16:54:24 +0500

    First CRUD application with api

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,67 @@
+class Api::V1::ProductsController < ApplicationController
+  def index
+
+    @record= Product.all
+
+    render json: @record, status:200
+  end
+
+  def show
+
+    @product=Product.find_by( params[:id])
+    if @product 
+      render json: @product, status:200
+    else
+      render json: {error:"Product not found "}
+    end
+
+  end
+
+  def create
+
+    @product=Product.new(
+      prod_params
+      
+    )
+
+    if @product.save
+      render json: @product, status:200
+    else
+      render json: {error:"Product not created"}
+    end
+
+  end
+
+  def update
+    @product=Product.find_by( params[:id])
+
+    if @product
+      @product.update(prod_params)
+      render json: @product, status:200
+    else
+      render json: {error:"Product is not able to update"}
+    end
+
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    
+    @product.destroy
+
+    render json: "Element deleted", ststus:200
+
+     
+  end
+
+
+  private
+  def prod_params
+
+    params.require(:product).permit([
+      :name, :brand, :price]
+      )
+  end
+
+  
+end
---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+   Product.create([{
+     name: 'milk', brand: 'Nestle', price: '100' }, 
+     {name: 'milk2', brand: 'Nestle', price: '200' }, 
+     {name: 'milk3', brand: 'Nestle', price: '300' }, 
+     {name: 'milk4', brand: 'Nestle', price: '400' }, 
+     {name: 'milk5', brand: 'Nestle', price: '500' }, 
+     {name: 'milk6', brand: 'Nestle', price: '600' }, 
+     {name: 'milk7', brand: 'Nestle', price: '700' }, 
+     {name: 'milk8', brand: 'Nestle', price: '800' }, 
+])
+   
+#   Character.create(name: "Luke", movie: movies.first)
